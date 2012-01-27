780c3f6c1481e83902a1661c380833803839fa32
Owen Derby <oderby@mit.edu>
2012-01-27 16:36:18 -0500

    Several changes - improve chucktesta, record both 2- and 3-player evs in MatchHistory and return appropriate one in average, clean up the code some more.

--- StephenBot/Action.py
+++ StephenBot/Action.py
@@ -2,13 +2,14 @@ from Enums import *
 
 class Action:
     def __init__(self, type, player, c1, c2, potAmt=0, betAmt=0, amt=0,
-                 handStrength=0):
+                 ev=0):
         self.player = player
         self.type = type
         self.potAmount = potAmt
         self.betAmount = betAmt
         self.amount = amt
-        self.handStrength = handStrength
+        #tuple, ev2,ev3
+        self.ev = ev
 
         self.showCard1 = c1
         self.showCard2 = c2
@@ -21,9 +22,9 @@ class Action:
                       self.potAmount,
                       self.betAmount,
                       self.amount,
-                      self.handStrength)
+                      self.ev)
 
     def __repr__(self):
-        return "%s %s [%3.f, %.3f, %.3f] %d" % (self.player, ACTION_TYPES[self.type],
+        return "%s %s [%3.f, %.3f, %.3f] [%d %d]" % (self.player, ACTION_TYPES[self.type],
                                           self.amount, self.betAmount,
-                                          self.potAmount, self.handStrength)
+                                          self.potAmount, self.ev)
