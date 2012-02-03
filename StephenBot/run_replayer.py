2a671119b545487f93a32891dfc0f66b42f2e258
Owen Derby <oderby@mit.edu>
2012-02-03 05:01:28 -0500

    small stuff

--- StephenBot/run_replayer.py
+++ StephenBot/run_replayer.py
@@ -1,17 +1,22 @@
-from HandHistoryStatsCalculator import HandHistoryRePlayer
+from HandHistoryRePlayer import HandHistoryRePlayer
 import multiprocessing as mp
 import os,sys
 
 def replayMatch(fname):
     if os.path.exists(fname):
-        h = HandHistoryRePlayer(fname)
+        h = HandHistoryRePlayer([fname])
         return h.run()
     else:
         print >> sys.stderr, fname + " doesn't exist!"
 
 if __name__ == "__main__":
     print sys.argv[1:]
-    pool = mp.Pool()
-    result = pool.map_async(replayMatch,sys.argv[1:])
-    for r in result.get():
-        print r
+    for f in sys.argv[1:]:
+        replayMatch(f)
+    #pool = mp.Pool()
+    #result = pool.map_async(replayMatch,sys.argv[1:])
+    #f_out = open("stats_normalized.txt", 'a')
+    #for r in result.get():
+    #    f_out.write(r)
+    #f_out.flush()
+    #f_out.close()
